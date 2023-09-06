e98d550b98717f9a3cf60a913bfef37c583ca664
rowanspain <125788793+rowanspain@users.noreply.github.com>
2023-09-06 16:42:27 -0700

    Lesson D

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // rate of forward/backward movement
     private float speed = 15.0f;
+    private float turnSpeed = 15.0f;
+
+    private float horizontalInput;
+    private float verticalInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,11 +20,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        
         Debug.Log(Time.deltaTime);
-        // Vector3.forward -- > (0, 0, 1)
-        // (0, 0, 1) * Time.deltaTime = (0, 0, .0016) * = (0, 0, .16)
-       transform.Translate(Vector3.forward * Time.deltaTime * speed);
 
-
+       transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+       // turning vehicle
+       transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
